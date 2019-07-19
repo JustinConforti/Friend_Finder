05f550cc0fe736c148847bccf19c6d93a737e804
Justin <justinconforti@gmail.com>
2019-07-19 10:21:12 -0400

    working on survey HTML

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 var express = require("express");
 var path = require("path")
+
+
 var PORT = process.env.PORT || 8080
+
 var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/App/public/home.html"));
-  });
-   
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/App/public/survey.html"));
-  });
 
+//require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.post("/api/friends", function(req, res) {
+
+      
+
+})
 
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
