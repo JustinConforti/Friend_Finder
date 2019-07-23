274da39fd148e897e9f907b6aad196de95af4973
Justin <justinconforti@gmail.com>
2019-07-23 17:33:48 -0400

    attempting to deploy to heroku

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
- let newUser = require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
-let userArrays = require("./app/data/friends")
+require("./app/data/friends")
 
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
